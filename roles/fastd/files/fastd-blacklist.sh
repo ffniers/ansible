bcb9999d05ceee1444f654279a19bb393449b952
Daniel Weuthen <daniel@weuthen-net.de>
2016-02-21 18:35:58 +0100

    Fixes mesh scripts Finished fastd

---
+++ roles/fastd/files/fastd-blacklist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PEER_KEY=$1
+
+if /bin/grep -Fq $PEER_KEY /etc/fastd/fastd-blacklist.json; then
+        exit 1
+elif /bin/grep -Fq $PEER_KEY /etc/fastd/fastd-localblock.keys; then
+        exit 1
+else
+        exit 0
+fi
